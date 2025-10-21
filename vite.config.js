58eb6731be1771ea4aeea6d69bc0acc7caad0f0c
kietnase173510 <kietnase173510@fpt.edu.vn>
2025-10-21 09:51:12 +0700

    change be

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 5173, 
     proxy: {
       '/api': {
-        target: 'http://localhost:3000', 
+        target: 'https://moccam-backend.onrender.com', 
         changeOrigin: true,               
         secure: false,                   
       },
