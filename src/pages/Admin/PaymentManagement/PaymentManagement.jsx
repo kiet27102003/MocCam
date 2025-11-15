ca90a62a4dbdf95aef4a8d083e1d63e1e05396ba
kietnase173510 <kietnase173510@fpt.edu.vn>
2025-11-15 22:27:42 +0700

    New

--- src/pages/Admin/PaymentManagement/PaymentManagement.jsx
+++ src/pages/Admin/PaymentManagement/PaymentManagement.jsx
@@ -114,8 +114,8 @@ const PaymentManagement = () => {
       year: 'numeric',
       month: '2-digit',
       day: '2-digit',
-      hour: '2-digit',
-      minute: '2-digit'
+      // hour: '2-digit',
+      // minute: '2-digit'
     });
   };
 
